64aa5cab5a7ece70ad898ef2a768a7bdc16d5b36
Barri2k18 <44354611+Barri2k18@users.noreply.github.com>
2018-11-14 16:42:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -104,7 +104,7 @@ async def rules(ctx):
 @bot.command(pass_context = True)
 async def giveaways(ctx):
 	if ctx.message.author.id not in owner:
-	embed=discord.Embed(colour = discord.Colour.orange(), title=" ")
+	embed=discord.Embed(title=" ")
 	embed.set_author(name="Giveaways", icon_url="https://media.discordapp.net/attachments/511226306692775936/511960131169419264/612f3fc9dedfd368820b55c4cf259c07.png")
 	embed.add_field(name="What do we giveaway?", value="Usually Tatsumaki credits or pokemons or even roles.")
 	embed.add_field(name="How to host giveaways?", value="You can host giveaways and make requirements for the giveaway. After someone has won they must claim their price in 48 hours or less.", inline=True)
