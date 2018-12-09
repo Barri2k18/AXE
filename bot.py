6258f528ac2e4954f9691229c45e44ff64a4107d
Barri2k18 <44354611+Barri2k18@users.noreply.github.com>
2018-12-09 17:58:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from discord.ext.commands.cooldowns import BucketType
 import asyncio
 import platform
 import colorsys
+import youtube_dl
 import random
 import os
 import time
@@ -14,6 +15,49 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 #cogs = storage/emulated/0/Android/Azone/Cogs
 
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+load_opus_lib()
+
+in_voice=[]
+
+
+players = {}
+songs = {}
+playing = {}
+
+
+async def all_false():
+    for i in bot.servers:
+        playing[i.id]=False
+
+
+async def checking_voice(ctx):
+    await asyncio.sleep(130)
+    if playing[ctx.message.server.id]== False:
+        try:
+            pos = in_voice.index(ctx.message.server.id)
+            del in_voice[pos]
+            server = ctx.message.server
+            voice_client = bot.voice_client_in(server)
+            await voice_client.disconnect()
+            await bot.say("{} left because there was no audio playing for a while".format(bot.user.name))
+        except:
+            pass
 
 owner = ["362672438699622403"]
 developer = ["362672438699622403"]
@@ -41,7 +85,107 @@ async def status_task():
         await bot.change_presence(game=discord.Game(name='Thot', type=1))
         await asyncio.sleep(1)
      
-         # ------------ Oofy      
+         # ------------ Oofy  
+	
+@bot.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    in_voice.append(ctx.message.server.id)
+
+
+async def player_in(con):  # After function for music
+    try:
+        if len(songs[con.message.server.id]) == 0:  # If there is no queue make it False
+            playing[con.message.server.id] = False
+            bot.loop.create_task(checking_voice(con))
+    except:
+        pass
+    try:
+        if len(songs[con.message.server.id]) != 0:  # If queue is not empty
+            # if audio is not playing and there is a queue
+            songs[con.message.server.id][0].start()  # start it
+            await bot.send_message(con.message.channel, 'Now queueed')
+            del songs[con.message.server.id][0]  # delete list afterwards
+    except:
+        pass
+        
+
+
+@bot.command(pass_context=True)
+async def play(ctx, *,url):
+
+    opts = {
+        'default_search': 'auto',
+        'quiet': True,
+    }  # youtube_dl options
+
+
+    if ctx.message.server.id not in in_voice: #auto join voice if not joined
+        channel = ctx.message.author.voice.voice_channel
+        await bot.join_voice_channel(channel)
+        in_voice.append(ctx.message.server.id)
+
+    
+
+    if playing[ctx.message.server.id] == True: #IF THERE IS CURRENT AUDIO PLAYING QUEUE IT
+        voice = bot.voice_client_in(ctx.message.server)
+        song = await voice.create_ytdl_player(url, ytdl_options=opts, after=lambda: bot.loop.create_task(player_in(ctx)))
+        songs[ctx.message.server.id]=[] #make a list 
+        songs[ctx.message.server.id].append(song) #add song to queue
+        await bot.say("Audio {} is queued".format(song.title))
+
+    if playing[ctx.message.server.id] == False:
+        voice = bot.voice_client_in(ctx.message.server)
+        player = await voice.create_ytdl_player(url, ytdl_options=opts, after=lambda: bot.loop.create_task(player_in(ctx)))
+        players[ctx.message.server.id] = player
+        # play_in.append(player)
+        if players[ctx.message.server.id].is_live == True:
+            await bot.say("Can not play live audio yet.")
+        elif players[ctx.message.server.id].is_live == False:
+            player.start()
+            await bot.say("Now playing audio")
+            playing[ctx.message.server.id] = True
+
+
+
+@bot.command(pass_context=True)
+async def queue(con):
+    await bot.say("There are currently {} audios in queue".format(len(songs)))
+
+@bot.command(pass_context=True)
+async def pause(ctx):
+    players[ctx.message.server.id].pause()
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    players[ctx.message.server.id].resume()
+          
+@bot.command(pass_context=True)
+async def volume(ctx, vol:float):
+    volu = float(vol)
+    players[ctx.message.server.id].volume=volu
+
+
+@bot.command(pass_context=True)
+async def skip(con): #skipping songs?
+  songs[con.message.server.id]
+    
+    
+    
+@bot.command(pass_context=True)
+async def stop(con):
+    players[con.message.server.id].stop()
+    songs.clear()
+
+@bot.command(pass_context=True)
+async def leave(ctx):
+    pos=in_voice.index(ctx.message.server.id)
+    del in_voice[pos]
+    server=ctx.message.server
+    voice_client=bot.voice_client_in(server)
+    await voice_client.disconnect()
+    songs.clear()
 
 #@bot.command(pass_context=True)
 #async def send1(ctx): #--- Not Working Because: This bot isn't hosted with my phone.
